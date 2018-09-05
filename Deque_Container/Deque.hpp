0dcc3b3f6dac014804a8f2e964b538904aab9e45
Roger Correia <rcorrei1@binghamton.edu>
2018-09-05 18:00:59 -0400

    initial set-up

--- Deque_Container/Deque.hpp
+++ Deque_Container/Deque.hpp
@@ -2,4 +2,5 @@
  * Implement your functionality here without changing test.cpp
  */
 
-
+#define Deque_DEFINE(type) \
+  int deque##type = 12;
---
+++ Deque_Container/sample.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Deque.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Deque.hpp"
+
+int main() {
+	Deque_DEFINE(int)
+	printf("%d", dequeint);
+	return 0;
+}
