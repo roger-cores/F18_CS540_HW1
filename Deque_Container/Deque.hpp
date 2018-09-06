8eec39a67e4928378e352e5b7f1e8b9e53292ae0
Roger Correia <rcorrei1@binghamton.edu>
2018-09-06 07:13:54 -0400

    added ctor

--- Deque_Container/Deque.hpp
+++ Deque_Container/Deque.hpp
@@ -1,6 +1,12 @@
 /*
  * Implement your functionality here without changing test.cpp
  */
-
-#define Deque_DEFINE(type) \
-  int deque##type = 12;
+#define Deque_DEFINE(type)                                                      \
+struct Deque_##type {                                                           \
+ int data;                                                                      \
+ bool (*comparator)(const type &one, const type &two);                          \
+};                                    \
+void Deque_##type##_ctor(Deque_##type *deq, bool (*comparator)(const type &one, const type &two)) { \
+  deq = (Deque_##type *) malloc(sizeof(Deque_##type));                          \
+  deq->comparator = comparator;                                                 \
+}
--- Deque_Container/sample.cpp
+++ Deque_Container/sample.cpp
@@ -1,13 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "Deque.hpp"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "Deque.hpp"
+/**
+  Type of data
+**/
+struct MyClass {
+    int id;
+    char name[10];
+};
+
+/**
+  A comparator
+**/
+bool
+MyClass_less_by_id(const MyClass &o1, const MyClass &o2) {
+    return o1.id < o2.id;
+}
+
+/**
+  expands code for MyClass type
+**/
+Deque_DEFINE(MyClass)
 
 int main() {
-	Deque_DEFINE(int)
-	printf("%d", dequeint);
+	Deque_MyClass deq;
+  Deque_MyClass_ctor(&deq, MyClass_less_by_id);
 	return 0;
 }
